4aeb2deb13f32e218e44d1a0e25090ab2b7f899f
WedsonTavares <wedsonsobral@gmail.com>
2025-07-18 15:46:58 -0300

    favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -56,9 +56,14 @@ export const metadata = {
     images: ["/og-image.png"],
   },
   icons: {
-    icon: "/favicon.ico"
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" }
+    ]
   }
-};
+}
 
 export default function RootLayout({ children }) {
   return (
